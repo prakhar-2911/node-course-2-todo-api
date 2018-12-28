6625243605d178ac7f36bfa2441b59cbca07ba59
Prakhar Sharma <you@example.com>
2018-12-28 22:25:11 +0530

    added GET todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 
 var {User} = require('./models/user');
@@ -34,6 +36,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.send(404).send();
+    }
+    res.send({todo});
+}).catch((e) => {
+    res.status(400).send();
+})
+
+
+
+
 });
 
 
