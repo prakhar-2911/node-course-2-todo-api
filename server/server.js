225ad5c8cd38e2c5457422df9e02546ea19b17b3
Prakhar Sharma <you@example.com>
2018-12-29 15:21:41 +0530

    heroku configured

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ var {Todo} = require('./models/todo');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -61,8 +63,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('starting on port 3000');
+app.listen(port, () => {
+  console.log(`starting on port ${port}`);
 });
 // // var newTodo = new Todo({
 // //   text: 'lets cook something'
