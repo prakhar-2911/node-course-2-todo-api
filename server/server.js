f3f591c179f993d7624ca120599f3bc770840206
Prakhar Sharma <you@example.com>
2018-12-31 01:30:22 +0530

    added PATCH /todos

--- server/server.js
+++ server/server.js
@@ -1,130 +1,176 @@
-var express = require('express');
+  var express = require('express');
 
-var bodyParser = require('body-parser');
+  var bodyParser = require('body-parser');
 
-var {ObjectID} = require('mongodb');
+  var _ = require('lodash');
 
-var {mongoose} = require('./db/mongoose');
+  var {ObjectID} = require('mongodb');
 
-var {User} = require('./models/user');
+  var {mongoose} = require('./db/mongoose');
 
-var {Todo} = require('./models/todo');
+  var {User} = require('./models/user');
 
-var app = express();
+  var {Todo} = require('./models/todo');
 
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-
-app.post('/todos', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
-
-  todo.save().then((doc) => {
-    res.send(doc);
-  },
-(e) => {
-  res.status(400).send(e);
-});
-
-});
+  var app = express();
 
 
+  const port = process.env.PORT || 3000;
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((doc) => {
-    res.send(doc);
-  }, (e) => {
+  app.use(bodyParser.json());
+
+  app.post('/todos', (req, res) => {
+    var todo = new Todo({
+      text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+      res.send(doc);
+    },
+  (e) => {
     res.status(400).send(e);
   });
-});
+
+  });
 
 
-// get todo api
 
-app.get('/todos/:id', (req, res) => {
-  var id = req.params.id;
+  app.get('/todos', (req, res) => {
+    Todo.find().then((doc) => {
+      res.send(doc);
+    }, (e) => {
+      res.status(400).send(e);
+    });
+  });
+
+
+  // get todo api
+
+  app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+      if(!todo){
+        return res.send(404).send();
+      }
+      res.send({todo});
+  }).catch((e) => {
+      res.status(400).send();
+  })
+
+  });
+
+
+  // delete todo api
+  /**
+
+   */
+
+  app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo){
-      return res.send(404).send();
+      return res.status(404).send();
     }
-    res.send({todo});
-}).catch((e) => {
-    res.status(400).send();
-})
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send()
+  });
 
-});
+  });
 
 
-// delete todo api
 
-app.delete('/todos/:id', (req, res) => {
+
+  /**
+   * patch todo api
+   */
+
+  app.patch('/todos/:id', (req, res) => {
+
   var id = req.params.id;
 
-if(!ObjectID.isValid(id)){
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+
+if(_.isBoolean(body.completed) && body.completed){
+  body.completedAt = new Date().getTime();
+}
+else{
+  body.completed = false;
+  body.completedAt = null;
+}
+
+Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
+
+if(!todo){
   return res.status(404).send();
 }
 
-Todo.findByIdAndRemove(id).then((todo) => {
-  if(!todo){
-    return res.status(404).send();
-  }
-  res.send(todo);
+res.send({todo});
 }).catch((e) => {
-  res.status(400).send()
+res.status(400).send();
+})
+
+
 });
 
-});
 
 
-
-app.listen(port, () => {
-  console.log(`starting on port ${port}`);
-});
-// // var newTodo = new Todo({
-// //   text: 'lets cook something'
-// // });
-// //
-// // newTodo.save().then((doc) => {
-// //   console.log(doc);
-// // },
-// // (err) => {
-// //   console.log(err);
-// // }
-// // )
-//
-// var otherTodo = new Todo({
-// text: '  whatsupp   ',
-// //completed: true,
-// //completedAt: 123
-// });
-//
-// otherTodo.save().then((doc) => {
-//   console.log(doc);
-// },
-// (err) => {
-//   console.log('Soemthing bad happened',err);
-// });
-//
-//
-//
-//
-//
-//
-// var user = new User({
-//   email: 'prakharcsharma@gmail.com'
-// });
-//
-//
-// user.save().then((doc)=> {
-// console.log(doc);
-// },
-// (e) => {
-//   console.log(e);
-// })
+  app.listen(port, () => {
+    console.log(`starting on port ${port}`);
+  });
+  // // var newTodo = new Todo({
+  // //   text: 'lets cook something'
+  // // });
+  // //
+  // // newTodo.save().then((doc) => {
+  // //   console.log(doc);
+  // // },
+  // // (err) => {
+  // //   console.log(err);
+  // // }
+  // // )
+  //
+  // var otherTodo = new Todo({
+  // text: '  whatsupp   ',
+  // //completed: true,
+  // //completedAt: 123
+  // });
+  //
+  // otherTodo.save().then((doc) => {
+  //   console.log(doc);
+  // },
+  // (err) => {
+  //   console.log('Soemthing bad happened',err);
+  // });
+  //
+  //
+  //
+  //
+  //
+  //
+  // var user = new User({
+  //   email: 'prakharcsharma@gmail.com'
+  // });
+  //
+  //
+  // user.save().then((doc)=> {
+  // console.log(doc);
+  // },
+  // (e) => {
+  //   console.log(e);
+  // })
